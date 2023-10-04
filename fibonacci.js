0fa5d23a080853cbc425019c685f6f4a73f8e046
minshallw12 <111906475+minshallw12@users.noreply.github.com>
2023-10-04 14:48:39 -0600

    solved

--- fibonacci.js
+++ fibonacci.js
@@ -1,5 +1,11 @@
 function fibonacci(num) {
-  return 0;
+  if (num === 0) {
+    return 0
+  } else if (num === 1) {
+    return 1
+  }
+
+  return fibonacci(num-1) + fibonacci(num-2);
 }
 
 module.exports = fibonacci;
