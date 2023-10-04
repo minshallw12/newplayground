925435781bb346d23edafd04918e716e67be085f
minshallw12 <111906475+minshallw12@users.noreply.github.com>
2023-10-04 15:20:05 -0600

    solved

--- algo-linear-search/practice/linearSearch.js
+++ algo-linear-search/practice/linearSearch.js
@@ -1,9 +1,22 @@
 function linearSearch(searchTerm, arr) {
+
+  for (i=0; i<arr.length; i++){
+      if (searchTerm === arr[i]) {
+        return i;
+      } 
+  }
   return undefined;
 }
 
 function globalLinearSearch(searchTerm, arr) {
-  return [];
+  const myLetters = [];
+  for (i=0; i<arr.length; i++) {
+    if (searchTerm === arr[i]) {
+      myLetters.push(i)
+    }
+  }
+
+  return myLetters;
 }
 
 module.exports = { linearSearch, globalLinearSearch };
